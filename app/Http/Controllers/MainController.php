3da0770d593699bf6e12d482f0b797c10f982a97
Mckennie <120336618+Mckennie@users.noreply.github.com>
2023-06-18 10:27:52 +0100

    Add files via upload

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Answer;
+use App\Models\Quiz;
+use App\Models\Result;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function dashboard()
+    {
+        if (!auth()->check()) {
+            // User is not authenticated, handle accordingly
+            // For example, you can redirect them to the login page
+            return redirect()->route('login');
+        }
+    
+        $quizzes = Quiz::where('status', 'publish')->where(function ($query) {
+            $query->whereNull('finished_at')->orWhere('finished_at', '>', now());
+        })->withCount('questions')->paginate(5);
+    
+        $results = auth()->user()->results;
+    
+        return view('dashboard', compact('quizzes', 'results'));
+    }
+    
+
+    public function quiz($slug)
+    {
+        $quiz = Quiz::whereSlug($slug)->with('questions.my_answer', 'my_result')->first() ?? abort(404, 'Quiz Not Found');
+        if ($quiz->my_result) {
+            return view('quiz_result', compact('quiz'));
+        }
+        return view('quiz', compact('quiz'));
+    }
+
+    public function quiz_detail($slug)
+    {
+        $quiz = Quiz::whereSlug($slug)->with('my_result', 'topTen.user')->withCount('questions')->first() ?? abort(404, 'Quiz Not Found');
+        return view('quiz_detail', compact('quiz'));
+    }
+
+    public function result(Request $request, $slug)
+    {
+        $quiz = Quiz::with('questions')->whereSlug($slug)->first() ?? abort(404, 'Quiz Not Found!');
+        $correct = 0;
+
+        if ($quiz->my_result) {
+            abort(404, 'You have participated in this Quiz before!');
+        }
+
+        foreach ($quiz->questions as $question) {
+            //echo $question->id . " - " . $question->correct_answer . "/" . $request->post($question->id) . "<br>";
+            Answer::create([
+                'user_id' => auth()->user()->id,
+                'question_id' => $question->id,
+                'answer' => $request->post($question->id)
+            ]);
+
+            // echo $question->correct_answer . " - " . $request->post($question->id) . "<br>";
+
+            if ($question->correct_answer === $request->post($question->id)) {
+                $correct += 1;
+            }
+        }
+
+        $point = round((100 / count($quiz->questions)) * $correct);
+        $wrong = count($quiz->questions) - $correct;
+
+        Result::create([
+            'user_id' => auth()->user()->id,
+            'quiz_id' => $quiz->id,
+            'point' => $point,
+            'correct' => $correct,
+            'wrong' => $wrong
+        ]);
+
+        return redirect()->route('quiz.detail', $quiz->slug)->withSuccess('You have successfully completed the quiz. Your score: ' . $point);
+    }
+}
